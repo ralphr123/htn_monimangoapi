c1c0def33cb5abe3e70892a2beb1e1b60d396285
Ralph Rouhana <rrouhana@sipstack.com>
2021-01-16 01:06:27 -0500

    initialized /tone IBM api call

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 const express = require("express");
+const ToneAnalyzerV3 = require('ibm-watson/tone-analyzer/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 
+
+const toneAnalyzer = new ToneAnalyzerV3({
+  version: '2017-09-21',
+  authenticator: new IamAuthenticator({
+    apikey: 'PXHHE9AYFhawG-py2XZxPjo8YFgxnSQTZE-_IEKDJyGe',
+  }),
+  serviceUrl: 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/265b8730-09a9-4f67-8328-a19ce9e9d4ab',
+  disableSslVerification: true,
+});
+
+const text = 'Team, I know that times are tough! Product '
+  + 'sales have been disappointing for the past three '
+  + 'quarters. We have a competitive product, but we '
+  + 'need to do a better job of selling it!';
+
+const toneParams = {
+  toneInput: { 'text': text },
+  contentType: 'application/json',
+};
+
 app.get('/', (req, res) => {
     res.send("This works.");
+});
+
+app.get('/tone', (req, res) => {
+    toneAnalyzer.tone(toneParams)
+        .then(toneAnalysis => {
+            res.send(JSON.stringify(toneAnalysis, null, 2));
+        })
+        .catch(err => {
+            console.log('error:', err);
+        });
 });
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
