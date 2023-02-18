7ec279883acc0fc202d1fa8510a6ccea9026f2c7
Ralph Rouhana <ralph.rouhana@gmail.com>
2023-02-18 13:57:40 -0800

    Another attempt to fix cors error

--- index.js
+++ index.js
@@ -109,6 +109,14 @@ app.get('/nexmo', (req, res) => {
   });
 });
 
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.get('/reddit', (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'application/json',
